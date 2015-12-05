f254b05107e725569c0c1d9815c1638e3ee25eb0
Alejandro Banzas <alebanzas@outlook.com>
2015-12-05 16:09:14 -0300

    arranco webjob que desencola

---
+++ DevDayKeynote/DevDayKeynote.Job/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Configuration;
+
+namespace DevDayKeynote.Job
+{
+    class Program
+    {
+        static void Main()
+        {
+            if (!VerifyConfiguration())
+            {
+                Console.ReadLine();
+                return;
+            }
+            
+
+
+            JobHost host = new JobHost();
+            host.RunAndBlock();
+        }
+
+        private static bool VerifyConfiguration()
+        {
+            var webJobsStorage = ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ConnectionString;
+
+            var configOK = true;
+            if (string.IsNullOrWhiteSpace(webJobsStorage))
+            {
+                configOK = false;
+                Console.WriteLine("Please add the Azure Storage account credentials in App.config");
+
+            }
+            return configOK;
+        }
+        
+    }
+}
